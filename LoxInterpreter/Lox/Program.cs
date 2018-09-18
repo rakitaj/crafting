574f70cb64e3852b3e3492bc78edd4c613ba1492
Joshua Rakita <joshuarakita@gmail.com>
2018-09-17 17:52:08 -0700

    Going to build the interpreter with C#.
    Need to figure out a replacement for the scanner class.

---
+++ LoxInterpreter/Lox/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Lox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Usage CSharpLox [script]");
+                Environment.Exit(64);
+            }
+            else if (args.Length == 1)
+            {
+                RunFile(args[0]);
+            }
+            else
+            {
+                RunPrompt();
+            }
+        }
+
+        public static void RunFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(Path.GetFullPath(path));
+            var source = System.Text.Encoding.Default.GetString(bytes);
+            Run(source);
+        }
+
+        public static void Run(string source)
+        {
+
+        }
+
+        public static void RunPrompt()
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                Run(Console.ReadLine());
+            }
+        }
+    }
+}
---
+++ prework/linkedlist.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *previous;
+};
+
+typedef struct Node Node_t;
+
+struct LinkedList {
+    Node_t *head;
+    Node_t *tail;
+};
+
+typedef struct LinkedList LinkedList_t;
+
+LinkedList_t* LinkedList_new() {
+    LinkedList_t *linked_list = malloc(sizeof(LinkedList_t));
+    return linked_list;
+}
+
+void LinkedList_add_to_front(LinkedList_t* linked_list) {
+    // do I need to dereference head and tail?
+}
