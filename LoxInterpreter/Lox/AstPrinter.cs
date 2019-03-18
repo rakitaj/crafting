977357862b87bea39ddbf19f3a13e79e2b1f5c4d
Joshua Rakita <joshuarakita@gmail.com>
2019-03-18 13:03:36 -0700

    Uncomment AstPrinter class now that missing base method is fixed.

--- LoxInterpreter/Lox/AstPrinter.cs
+++ LoxInterpreter/Lox/AstPrinter.cs
@@ -1,42 +1,42 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace Lox
-//{
-//    public class AstPrinter : IVisitor<String>
-//    {
-//        public string VisitBinaryExpr(Binary expr)
-//        {
-//            return this.Parenthesize(expr.operation.Lexeme, expr.left, expr.right);
-//        }
+namespace Lox
+{
+    public class AstPrinter : IVisitor<String>
+    {
+        public string VisitBinaryExpr(Binary expr)
+        {
+            return this.Parenthesize(expr.operation.Lexeme, expr.left, expr.right);
+        }
 
-//        public string VisitGroupingExpr(Grouping expr)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public string VisitGroupingExpr(Grouping expr)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public string VisitLiteralExpr(Literal expr)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public string VisitLiteralExpr(Literal expr)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public string VisitUnaryExpr(Unary expr)
-//        {
-//            throw new NotImplementedException();
-//        }
+        public string VisitUnaryExpr(Unary expr)
+        {
+            throw new NotImplementedException();
+        }
 
-//        public string Parenthesize(string name, params Expr[] expressions)
-//        {
-//            StringBuilder builder = new StringBuilder();
-//            builder.Append("(").Append(name);
-//            foreach(Expr expr in expressions)
-//            {
-//                builder.Append("");
-//                builder.Append(expr.Accept(this));
-//            }
-//            builder.Append(")");
-//            return builder.ToString();
-//        }
-//    }
-//}
+        public string Parenthesize(string name, params Expr[] expressions)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("(").Append(name);
+            foreach (Expr expr in expressions)
+            {
+                builder.Append("");
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+            return builder.ToString();
+        }
+    }
+}
