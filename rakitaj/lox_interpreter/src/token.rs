a0033bd0bd3637a3015f6617d33e5ed5784decee
Joshua Rakita <joshuarakita@gmail.com>
2021-05-24 23:10:33 -0700

    New parsing working for single char tokens!

--- rakitaj/lox_interpreter/src/token.rs
+++ rakitaj/lox_interpreter/src/token.rs
@@ -1,26 +1,54 @@
 use std::fmt;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum TokenType {
-    // Single-character tokens.                      
-    LeftParen, RightParen, LeftBrace, RightBrace,
-    Comma, Dot, Minus, Plus, SemiColon, Slash, Star,
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    SemiColon,
+    Slash,
+    Star,
 
-    // One or two character tokens.                  
-    Bang, BangEqual,
-    Equal, EqualEqual,
-    Greater, GreaterEqual,
-    Less, LessEqual,
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
-    // Literals.                                     
-    Identifier(String), String(String), Number(f32),
+    // Literals.
+    Identifier(String),
+    String(String),
+    Number(f32),
 
-    // Keywords.                                     
-    And, Class, Else, False, Fun, For, If, Nil, Or,
-    Print, Return, Super, This, True, Var, WHile,
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    WHile,
 
-    Eof
+    Eof,
 }
 
 impl fmt::Display for TokenType {
@@ -29,24 +57,46 @@ impl fmt::Display for TokenType {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Token {
     token_type: TokenType,
     line: usize,
     lexeme: String,
-    literal: String
+    literal: Option<String>,
+}
+
+impl Token {
+    pub fn new(
+        token_type: TokenType,
+        line: usize,
+        lexeme: String,
+        literal: Option<String>,
+    ) -> Self {
+        Token {
+            token_type: token_type,
+            line: line,
+            lexeme: lexeme,
+            literal: literal,
+        }
+    }
 }
 
 impl fmt::Display for Token {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Use `self.number` to refer to each positional data point.
-        write!(f, "{} {} {}", &self.token_type, &self.lexeme, &self.literal)
+        write!(
+            f,
+            "{} {} {}",
+            &self.token_type,
+            &self.lexeme,
+            &self.literal.as_ref().unwrap_or(&"No literal".to_string())
+        )
     }
 }
 
 pub struct SourceCode {
     pub source: String,
     pub index: usize,
-    pub line: usize
+    pub line: usize,
 }
 
 impl SourceCode {
@@ -54,7 +104,7 @@ impl SourceCode {
         SourceCode {
             source: source,
             index: 0,
-            line: 1
+            line: 1,
         }
     }
 
@@ -69,82 +119,42 @@ impl SourceCode {
     pub fn eof(&self) -> bool {
         match &self.get() {
             Some(_) => false,
-            None => true
+            None => true,
         }
     }
 
-    pub fn next_token(&mut self) -> Option<TokenType> {
-        let current_char = &self.get()?;
-        match current_char {
-            // Handle newline
-            '\n' => { self.index += 1; self.line += 1; },
-            // One character tokens.
-            '(' => { self.index += 1; return Some(TokenType::LeftParen) },
-            ')' => { self.index += 1; return Some(TokenType::RightParen) },
-            '{' => { self.index += 1; return Some(TokenType::LeftBrace) },
-            '}' => { self.index += 1; return Some(TokenType::RightBrace) },
-            ',' => { self.index += 1; return Some(TokenType::Comma) },
-            '.' => { self.index += 1; return Some(TokenType::Dot) },
-            '-' => { self.index += 1; return Some(TokenType::Minus) },
-            '+' => { self.index += 1; return Some(TokenType::Plus) },
-            ';' => { self.index += 1; return Some(TokenType::SemiColon) },
-            '*' => { self.index += 1; return Some(TokenType::Star) },
-            '/' => { self.index += 1; return Some(TokenType::Slash) },
-
-            // One or two character tokens.
-            // Bang, BangEqual,
-            '!' => {
-                if self.peek(1) == Some('=') {
-                    self.index += 2;
-                    return Some(TokenType::BangEqual)
-                } else {
-                    self.index += 1;
-                    return Some(TokenType::Bang)
+    pub fn scan_tokens(&mut self) -> Vec<Token> {
+        let mut tokens: Vec<Token> = Vec::new();
+        while let Some(c) = self.get() {
+            let start = self.index;
+            self.index += 1;
+            match c {
+                ' ' => continue,
+                '\t' => continue,
+                '\n' => {
+                    self.line += 1;
+                    continue;
                 }
-            },
-            // Equal, EqualEqual,
-            '=' => {
-                if self.peek(1) == Some('=') {
-                    self.index += 2;
-                    return Some(TokenType::EqualEqual)
-                } else {
-                    self.index += 1;
-                    return Some(TokenType::Equal)
-                }
-            },
-            // Greater, GreaterEqual,
-            '>' => {
-                if self.peek(1) == Some('=') {
-                    self.index += 2;
-                    return Some(TokenType::GreaterEqual)
-                } else {
-                    self.index += 1;
-                    return Some(TokenType::Greater)
-                }
-            },
-            // Less, LessEqual,
-            '<' => {
-                if self.peek(1) == Some('=') {
-                    self.index += 2;
-                    return Some(TokenType::LessEqual)
-                } else {
-                    self.index += 1;
-                    return Some(TokenType::Less)
-                }
-            } 
-            _ => return None
+                '(' => tokens.push(Token::new(TokenType::LeftParen, self.line, self.source[start..self.index].to_string(), None)),
+                ')' => tokens.push(Token::new(TokenType::RightParen, self.line, self.source[start..self.index].to_string(), None)),
+                '{' => tokens.push(Token::new(TokenType::LeftBrace, self.line, self.source[start..self.index].to_string(), None)),
+                '}' => tokens.push(Token::new(TokenType::RightBrace, self.line, self.source[start..self.index].to_string(), None)),
+                ',' => tokens.push(Token::new(TokenType::Comma, self.line, self.source[start..self.index].to_string(), None)),
+                '.' => tokens.push(Token::new(TokenType::Dot, self.line, self.source[start..self.index].to_string(), None)),
+                '-' => tokens.push(Token::new(TokenType::Minus, self.line, self.source[start..self.index].to_string(), None)),
+                '+' => tokens.push(Token::new(TokenType::Plus, self.line, self.source[start..self.index].to_string(), None)),
+                ';' => tokens.push(Token::new(TokenType::SemiColon, self.line, self.source[start..self.index].to_string(), None)),
+                '*' => tokens.push(Token::new(TokenType::Star, self.line, self.source[start..self.index].to_string(), None)),
+                '/' => tokens.push(Token::new(TokenType::Slash, self.line, self.source[start..self.index].to_string(), None)),
+                _ => continue,
+            }
         }
+        return tokens;
     }
 }
 
-pub fn lex(mut source: SourceCode) -> Vec<TokenType> {
-    let mut tokens: Vec<TokenType> = Vec::new();
-    let mut token = source.next_token();
-    while let Some(t) = token {
-        tokens.push(t);
-        token = source.next_token();
-    }
-    return tokens;
+pub fn lex(mut source: SourceCode) -> Vec<Token> {
+    return source.scan_tokens();
 }
 
 #[cfg(test)]
@@ -167,22 +177,33 @@ mod tests {
     }
 
     #[test]
-    fn test_simple_next_token() {
+    fn test_scan_tokens_single_token() {
         let mut source = SourceCode::new("+".to_string());
-        let token = source.next_token().unwrap();
-        assert_eq!(token, TokenType::Plus);
+        let tokens = source.scan_tokens();
+        let token = Token::new(TokenType::Plus, 1, "+".to_string(), None);
+        assert_eq!(tokens, vec![token]);
     }
 
-    #[rstest]
-    #[case("!=", Some(TokenType::BangEqual))]
-    #[case("!", Some(TokenType::Bang))]
-    #[case("<=", Some(TokenType::LessEqual))]
-    #[case("<", Some(TokenType::Less))]
-    #[case(">=", Some(TokenType::GreaterEqual))]
-    #[case(">", Some(TokenType::Greater))]
-    fn test_next_token(#[case] raw_source: String, #[case] expected_token: Option<TokenType>) {
-        let mut source_code = SourceCode::new(raw_source);
-        let token = source_code.next_token();
-        assert_eq!(token, expected_token);
+    #[test]
+    fn test_scan_tokens_multiple_tokens() {
+        let mut source = SourceCode::new("+  - /".to_string());
+        let tokens = source.scan_tokens();
+        assert_eq!(tokens, vec![
+            Token::new(TokenType::Plus, 1, "+".to_string(), None), 
+            Token::new(TokenType::Minus, 1, "-".to_string(), None), 
+            Token::new(TokenType::Slash, 1, "/".to_string(), None)]);
     }
+
+    // #[rstest]
+    // #[case("!=", Some(TokenType::BangEqual))]
+    // #[case("!", Some(TokenType::Bang))]
+    // #[case("<=", Some(TokenType::LessEqual))]
+    // #[case("<", Some(TokenType::Less))]
+    // #[case(">=", Some(TokenType::GreaterEqual))]
+    // #[case(">", Some(TokenType::Greater))]
+    // fn test_next_token(#[case] raw_source: String, #[case] expected_token: Option<TokenType>) {
+    //     let mut source_code = SourceCode::new(raw_source);
+    //     let token = source_code.next_token();
+    //     assert_eq!(token, expected_token);
+    // }
 }
