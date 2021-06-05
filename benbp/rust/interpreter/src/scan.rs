4e8d23c2c7a5c60b14753acea76c55c33ff8c70f
Ben Broderick Phillips <bebroder@microsoft.com>
2021-06-05 01:56:42 -0400

    Draft at parsing tokens with lookahead support

--- benbp/rust/interpreter/src/scan.rs
+++ benbp/rust/interpreter/src/scan.rs
@@ -1,7 +1,10 @@
+use crate::token::Token;
+use crate::token::TokenType;
+
 pub struct SourceContext {
     source: String,
     idx: usize,
-    line: i32
+    line: i32,
 }
 
 impl SourceContext {
@@ -15,24 +18,81 @@ pub fn new_source(program: String) -> SourceContext {
     SourceContext {
         source: program,
         idx: 0,
-        line: 0
+        line: 0,
     }
 }
 
 pub fn tokenize(source: SourceContext) {
-    // println!("{}", source.source);
     scan_token(source)
 }
 
 fn scan_token(source: SourceContext) {
     let mut ctx = source;
     let mut curr = ctx.source.char_indices();
+    let mut tokens: Vec<Token> = Vec::new();
+
     while let Some((idx, c)) = curr.next() {
         match c {
+            '(' => tokens.push(Token::new(TokenType::LeftParen, "(", ctx.line, None)),
+            ')' => tokens.push(Token::new(TokenType::RightParen, ")", ctx.line, None)),
+            '{' => tokens.push(Token::new(TokenType::LeftBrace, "{", ctx.line, None)),
+            '}' => tokens.push(Token::new(TokenType::RightBrace, "}", ctx.line, None)),
+            ',' => tokens.push(Token::new(TokenType::Comma, ",", ctx.line, None)),
+            '.' => tokens.push(Token::new(TokenType::Dot, ".", ctx.line, None)),
+            '_' => tokens.push(Token::new(TokenType::Minus, "_", ctx.line, None)),
+            '+' => tokens.push(Token::new(TokenType::Plus, "+", ctx.line, None)),
+            ';' => tokens.push(Token::new(TokenType::Semicolon, ";", ctx.line, None)),
+            '*' => tokens.push(Token::new(TokenType::Star, "*", ctx.line, None)),
+
+            // Add peek for combos
+            '!' => {
+                // TODO: pass reference to curr down to func, so we can set curr to the curr clone
+                // if there is a match, as we need to increment past the matched lookahead
+                //let ttype = type_from_lookahead(curr.clone().next(), '=', TokenType::BangEqual, TokenType::Bang);
+                // TODO: lexeme needs to encompass all characters
+                //tokens.push(Token::new(ttype, "", ctx.line, None))
+
+                let mut clone = curr.clone();
+                let next = clone.next();
+                match next {
+                    Some((_, '=')) => {
+                        curr = clone; // advance iterator past lexeme
+                        tokens.push(Token::new(TokenType::BangEqual, "!=", ctx.line, None));
+                    }
+                    _ => tokens.push(Token::new(TokenType::Bang, "!", ctx.line, None)),
+                };
+            }
+
+            '\n' => ctx.line += 1,
             _ => {
                 ctx.idx = idx;
-                println!("DEFAULT {} at {}", c, idx)
+                // println!("DEFAULT {} at {}", c, idx)
+                print!("{}", c);
             }
         }
+    }
+
+    for t in tokens {
+        println!("t: {}", t.lexeme);
+    }
+
+    println!("");
+}
+
+fn type_from_lookahead(
+    peek_val: Option<(usize, char)>,
+    comparator: char,
+    match_type: TokenType,
+    no_match_type: TokenType,
+) -> TokenType {
+    match peek_val {
+        Some((_, peek_char)) => {
+            if peek_char == comparator {
+                match_type
+            } else {
+                no_match_type
+            }
+        }
+        _ => no_match_type,
     }
 }
--- benbp/rust/interpreter/src/token.rs
+++ benbp/rust/interpreter/src/token.rs
@@ -1,39 +1,95 @@
-use std::fmt;
+// use std::fmt;
 
-impl<T> fmt::Display for Token<T>
-where T: fmt::Display
-{
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.literal)
+// impl fmt::Display for Token {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         match self.literal {
+//             Some(l) => { write!(f, "{}", self.literal) }
+//             None => { write!(f, "None") }
+//         }
+// }
+//
+// impl fmt::Display for Literal {
+//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         write!(f, "{}", self)
+//     }
+// }
+
+#[allow(dead_code)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: &'static str,
+    pub line: i32,
+    pub literal: Option<Literal>,
+}
+
+impl Token {
+    pub fn new(
+        token_type: TokenType,
+        lexeme: &'static str,
+        line: i32,
+        literal: Option<Literal>,
+    ) -> Token {
+        Token {
+            token_type,
+            lexeme,
+            line,
+            literal,
+        }
     }
 }
 
-#[allow(dead_code)]
-pub struct Token<T> {
-    token_type: TokenType,
-    lexeme: String,
-    literal: T,
-    line: i32
+pub enum Literal {
+    I32Literal(i32),
+    StringLiteral(String),
 }
 
 #[allow(dead_code)]
 pub enum TokenType {
-  // Single-character tokens.
-  LeftParen, RightParen, LeftBrace, RightBrace,
-  Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
 
-  // One or two character tokens.
-  Bang, BangEqual,
-  Equal, EqualEqual,
-  Greater, GreaterEqual,
-  Less, LessEqual,
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
 
-  // Literals.
-  Identifier, StringLiteral, Number,
+    // Literals.
+    Identifier,
+    StringLiteral,
+    Number,
 
-  // Keywords.
-  And, Class, Else, False, Fun, For, If, Nil, Or,
-  Print, Return, Super, This, True, Var, While,
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
 
-  EOF
+    EOF,
 }
