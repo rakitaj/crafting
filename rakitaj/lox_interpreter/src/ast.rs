a043091a3ee6eef18c12c1f10909bc477474a9ae
Joshua Rakita <joraki@microsoft.com>
2021-10-25 20:20:49 -0700

    Start on AST printer.

--- rakitaj/lox_interpreter/src/ast.rs
+++ rakitaj/lox_interpreter/src/ast.rs
@@ -1,6 +1,3 @@
-// Questions
-// 1. How do I represent a tree of different types in rust?
-
 use crate::tokens::Token;
 
 pub enum Expr {
@@ -16,3 +13,18 @@ pub enum Expr {
 pub struct Ast {
     pub node: Expr
 }
+
+fn print_ast(ast: Ast) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    // print_ast_helper(Some(&ast.node), &result);
+    return result;
+}
+
+// fn print_ast_helper(node: Option<&Expr>, mut result: &Vec<String>) {
+//     match node {
+//         Some(expr) => match expr {
+//             Expr::Binary(left, op, right) => format!()
+//         },
+//         None => {}
+//     }
+// }
