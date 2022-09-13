be3656a6d3754f103372f361034671c72062df1b
Joshua Rakita <joshuarakita@gmail.com>
2022-09-13 14:38:46 -0700

    Building out binary matches in the interpreter.

--- rakitaj/lox_interpreter/src/interpreter.rs
+++ rakitaj/lox_interpreter/src/interpreter.rs
@@ -1,12 +1,15 @@
 use crate::parser::{Expr, Literal};
-use crate::value::Value;
+use crate::tokens::TokenType;
+use crate::value::{Value, is_truthy};
 
 pub struct Interpreter {
     root_expr: Expr
 }
 
 pub enum InterpreterError {
-    Blah
+    ValueTypeError(Value, String),
+    ExprUnaryMismatch(TokenType, String),
+    GenericError(String)
 }
 
 impl Interpreter {
@@ -14,24 +17,48 @@ impl Interpreter {
         Interpreter { root_expr }
     }
 
-    pub fn visit_expr(&self, expr: Expr) -> Value {
+    pub fn visit_expr(&self, expr: Expr) -> Result<Value, InterpreterError> {
         match expr {
             Expr::Literal(literal) => {
                 match literal {
-                    Literal::Nil => Value::Nil,
-                    Literal::False => Value::False,
-                    Literal::True => Value::True,
-                    Literal::Number(number) => Value::Number(number),
-                    Literal::String(string) => Value::String(string),
+                    Literal::Nil => Ok(Value::Nil),
+                    Literal::False => Ok(Value::False),
+                    Literal::True => Ok(Value::True),
+                    Literal::Number(number) => Ok(Value::Number(number)),
+                    Literal::String(string) => Ok(Value::String(string)),
                 }
             },
             Expr::Grouping(grouping) => self.visit_expr(*grouping),
-            Expr::Unary(token_type, unary) => {
-                let right: Value = self.visit_expr(*unary);
-                match token_type {
-                    TokenType::Minus => 
+            Expr::Unary(operator, unary) => {
+                let right: Value = self.visit_expr(*unary)?;
+                match operator {
+                    TokenType::Minus => {
+                        match right {
+                            Value::Number(number) => Ok(Value::Number(-number)),
+                            _ =>  {
+                                let error_msg = format!("Expected Value::Number and got {:?}", right);
+                                Err(InterpreterError::ValueTypeError(right, error_msg))
+                            }
+                        }
+                    },
+                    TokenType::Bang => {
+                        // Negate, aka flip, the result of is_truthy because this is the bang unary operator.
+                        match is_truthy(right) {
+                            true => Ok(Value::False),
+                            false => Ok(Value::True)
+                        }
+                    },
+                    _ => Err(InterpreterError::ExprUnaryMismatch(operator, "Visiting Unary".to_string()))
                 }
-            }
+            },
+            Expr::Binary(left_expr, operator, right_expr) => {
+                let left = self.visit_expr(left_expr)?;
+                let right = self.visit_expr(right_expr)?;
+                match operator {
+                    
+                }
+            },
+            _ => Err(InterpreterError::GenericError("Shouldn't get here".to_string()))
         }
     }
 }
--- rakitaj/lox_interpreter/src/value.rs
+++ rakitaj/lox_interpreter/src/value.rs
@@ -1,7 +1,30 @@
+#[derive(Debug)]
 pub enum Value {
     Nil,
     True,
     False,
     Number(f32),
     String(String),
+}
+
+pub fn is_truthy(value: Value) -> bool {
+    match value {
+        Value::False | Value::Nil => false,
+        _ => true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[rstest]
+    #[case(Value::Nil, false)]
+    #[case(Value::False, false)]
+    #[case(Value::True, true)]
+    #[case(Value::String("".to_string()), true)]
+    fn test_is_truthy(#[case] value: Value, #[case] expected: bool) {
+        assert_eq!(is_truthy(value), expected);
+    }
 }
