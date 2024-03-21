89f29121d0896623f4877749b8d938b6cfcaedab
Joshua Rakita <joshuarakita@gmail.com>
2024-03-20 21:58:23 -0700

    Main chunk of the file renames.

--- rakitaj/prework/linkedlist_wrapper.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-
-class Node(ctypes.Structure):
-
-    _fields_ = [
-        ("data", ctypes.c_int),
-        ("next", ctypes.pointer)
-    ]
--- rakitaj/prework/z_clean.ps1
+++
@@ -1,8 +0,0 @@
-Set-Location "~\projects\crafting\prework"
-
-Remove-Item "linkedlist.obj" -ErrorAction SilentlyContinue
-Remove-Item "linkedlist.dll" -ErrorAction SilentlyContinue
-Remove-Item "linkedlist.exe" -ErrorAction SilentlyContinue
-
-Remove-Item "linkedlist_tests.obj" -ErrorAction SilentlyContinue
-Remove-Item "linkedlist_tests.exe" -ErrorAction SilentlyContinue
